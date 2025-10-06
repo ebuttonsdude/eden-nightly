af1a2afe564bd3e0f700e877f182bb6cc32be0d2
Escary <81011361+pflyly@users.noreply.github.com>
2025-10-06 20:16:01 +0100

    runtime/clang-20

--- BSD-dependencies.sh
+++ BSD-dependencies.sh
@@ -9,7 +9,7 @@ case "$TARGET" in
                 ninja openssl opus pkgconf qt6-base qt6ct qt6-tools qt6-translations qt6-wayland sdl2 unzip vulkan-tools vulkan-loader wget zip zstd
     ;;
   Solaris)
-    pkg install git cmake developer/gcc-14 developer/build/ninja developer/clang-20
+    pkg install git cmake developer/gcc-14 developer/build/ninja runtime/clang-20
     pkg install qt6 libzip openssl zlib compress/zstd unzip pkg-config nasm autoconf mesa library/libdrm
     ;;
 esac
