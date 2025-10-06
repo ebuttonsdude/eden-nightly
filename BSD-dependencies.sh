464ee28d05a8f0a93277c16e293f5808accd235a
Escary <81011361+pflyly@users.noreply.github.com>
2025-10-06 20:04:42 +0100

    more deps

--- BSD-dependencies.sh
+++ BSD-dependencies.sh
@@ -5,11 +5,11 @@ case "$TARGET" in
   FreeBSD)
     sed -i '' -e 's/quarterly/latest/' /etc/pkg/FreeBSD.conf
     export ASSUME_ALWAYS_YES=true
-    pkg install -y autoconf bash boost-libs catch2 cmake ccache ffmpeg gcc gmake git glslang libfmt libzip nasm \
-                ninja pkgconf qt6-base qt6ct qt6-tools qt6-translations qt6-wayland sdl2 unzip vulkan-tools vulkan-loader wget zip
+    pkg install -y autoconf bash boost-libs catch2 cmake ffmpeg gcc gmake git glslang libfmt libzip nasm llvm20 \
+                ninja openssl opus pkgconf qt6-base qt6ct qt6-tools qt6-translations qt6-wayland sdl2 unzip vulkan-tools vulkan-loader wget zip zstd
     ;;
   Solaris)
-    pkg install git cmake developer/gcc-14 developer/build/ninja
+    pkg install git cmake developer/gcc-14 developer/build/ninja developer/clang-20
     pkg install qt6 libzip openssl zlib compress/zstd unzip pkg-config nasm autoconf mesa library/libdrm
     ;;
 esac
