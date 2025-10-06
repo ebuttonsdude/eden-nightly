d27afd176520d9f2a3f41e2a1c077291ff6f8b0f
Escary <81011361+pflyly@users.noreply.github.com>
2025-10-06 20:41:56 +0100

    test

--- BSD-dependencies.sh
+++ BSD-dependencies.sh
@@ -9,7 +9,10 @@ case "$TARGET" in
                 ninja openssl opus pkgconf qt6-base qt6ct qt6-tools qt6-translations qt6-wayland sdl2 unzip vulkan-tools vulkan-loader wget zip zstd
     ;;
   Solaris)
-    pkg install git cmake developer/gcc-14 developer/build/ninja runtime/clang-20
+    pkg install git cmake developer/gcc-14 developer/build/ninja
     pkg install qt6 libzip openssl zlib compress/zstd unzip pkg-config nasm autoconf mesa library/libdrm
+    pkg set-publisher -g https://pkg.openindiana.org/hipster openindiana
+    pkg refresh
+    pkg install openindiana/runtime/clang-20
     ;;
 esac
